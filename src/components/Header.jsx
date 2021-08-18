1730b418dcc483711d445c79ef9c2a842b472b54
Ruben Meza <rmezar@gmail.com>
2021-08-17 22:17:42 -0500

    add Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import mealsImg from '../assets/img/meals.jpg'
+import classes from './Header.module.css'
+
+export const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImg} alt="A table full ofo delicious food!" />
+      </div>
+    </>
+  )
+}
